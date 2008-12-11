c59e5b459aad8612c049684af0f8f2e9d06a4b58
Norman Clarke <norman@randomba.org>
2008-12-11 16:24:29 -0200

    Added sequence and scope fields to slugs; removed updated_at since slugs don't get updated.

--- generators/friendly_id_migration/templates/create_slugs.rb
+++ generators/friendly_id_migration/templates/create_slugs.rb
@@ -2,11 +2,13 @@ class CreateSlugs < ActiveRecord::Migration
   def self.up
     create_table :slugs do |t|
       t.string :name
-      t.string :sluggable_type
       t.integer :sluggable_id
-      t.timestamps
+      t.integer :sequence, :null => false, :default => 1
+      t.string :sluggable_type, :limit => 40
+      t.string :scope, :limit => 40
+      t.datetime :created_at
     end
-    add_index :slugs, [:name, :sluggable_type], :unique => true
+    add_index :slugs, [:name, :sluggable_type, :scope, :sequence], :unique => true
     add_index :slugs, :sluggable_id
   end
 
--- test/schema.rb
+++ test/schema.rb
@@ -15,15 +15,15 @@ ActiveRecord::Schema.define(:version => 3) do
   end
 
   create_table "slugs", :force => true do |t|
-    t.string   "name"
-    t.string   "sluggable_type"
-    t.integer  "sluggable_id"
+    t.string "name"
+    t.integer "sluggable_id"
+    t.integer "sequence", :null       => false, :default => 1
+    t.string "sluggable_type", :limit => 40
+    t.string "scope", :limit          => 40
     t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
-  add_index "slugs", ["sluggable_id"], :name => "index_slugs_on_sluggable_id"
-  add_index "slugs", ["name", "sluggable_type"], 
-    :name => "index_slugs_on_name_and_sluggable_type", :unique => true
+  add_index "slugs", ["sluggable_id"], :name           => "index_slugs_on_sluggable_id"
+  add_index "slugs", ["name", "sluggable_type", "scope", "sequence"], :name => "index_slugs_on_n_s_s_and_s", :unique => true
 
 end
