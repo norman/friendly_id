abcb9ddd28d1ba713953562c4dff129be7a9612d
Norman Clarke <norman@njclarke.com>
2009-10-19 11:15:39 -0300

    Updated version and changelog.

--- lib/friendly_id/version.rb
+++ lib/friendly_id/version.rb
@@ -3,8 +3,8 @@
 module FriendlyId #:nodoc:
   module Version #:nodoc:
     MAJOR = 2
-    MINOR = 1
-    TINY = 4
+    MINOR = 2
+    TINY = 0
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
 end
