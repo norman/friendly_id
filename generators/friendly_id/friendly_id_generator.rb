a337a36d8ba15a7796408c865cfebc3d550129eb
Joshua Nichols <josh@technicalpickles.com>
2009-11-11 15:58:39 +0800

    Make sure lib/tasks exists before putting a file ther.

--- generators/friendly_id/friendly_id_generator.rb
+++ generators/friendly_id/friendly_id_generator.rb
@@ -5,6 +5,7 @@ class FriendlyIdGenerator < Rails::Generator::Base
         m.migration_template(
           'create_slugs.rb', 'db/migrate', :migration_file_name => 'create_slugs'
         )
+        m.directory "lib/tasks"
         m.file "/../../../lib/tasks/friendly_id.rake", "lib/tasks/friendly_id.rake"
       end
     end
