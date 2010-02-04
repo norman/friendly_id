8d9d7e29b8a77b29adbfd83da41423a746989d5b
Norman Clarke <norman@njclarke.com>
2010-02-04 13:34:36 -0300

    Set up benchmarks to run everything.

--- extras/bench.rb
+++ extras/bench.rb
@@ -1,59 +1,59 @@
 #!/usr/bin/env ruby -KU
 require File.dirname(__FILE__) + '/extras'
 require 'rbench'
-FACTOR = 20
+FACTOR = 100
 RBench.run(TIMES) do
 
   column :times
   column :ar
 
-  # report 'find model using id', (TIMES * FACTOR).ceil do
-  #   ar { User.find(get_id) }
-  # end
-  # 
+  report 'find model using id', (TIMES * FACTOR).ceil do
+    ar { User.find(get_id) }
+  end
+
   report 'find model using array of ids', (TIMES * FACTOR).ceil do
     ar { User.find([get_id, get_id]) }
   end
 
-  # report 'find unslugged model using friendly id', (TIMES * FACTOR).ceil do
-  #   ar { User.find(USERS.rand) }
-  # end
-  # 
+  report 'find unslugged model using friendly id', (TIMES * FACTOR).ceil do
+    ar { User.find(USERS.rand) }
+  end
+
   report 'find unslugged model using array of friendly ids', (TIMES * FACTOR).ceil do
     ar { User.find([USERS.rand, USERS.rand]) }
   end
 
-  # report 'find slugged model using friendly id', (TIMES * FACTOR).ceil do
-  #   ar { Post.find(POSTS.rand) }
-  # end
-  # 
+  report 'find slugged model using friendly id', (TIMES * FACTOR).ceil do
+    ar { Post.find(POSTS.rand) }
+  end
+
   report 'find slugged model using array of friendly ids', (TIMES * FACTOR).ceil do
     ar { Post.find([POSTS.rand, POSTS.rand]) }
   end
 
-  # report 'find cached slugged model using friendly id', (TIMES * FACTOR).ceil do
-  #   ar { District.find(DISTRICTS.rand) }
-  # end
-  # 
+  report 'find cached slugged model using friendly id', (TIMES * FACTOR).ceil do
+    ar { District.find(DISTRICTS.rand) }
+  end
+
   report 'find cached slugged model using array of friendly ids', (TIMES * FACTOR).ceil do
     ar { District.find([DISTRICTS.rand, DISTRICTS.rand]) }
   end
 
-  # report 'find model using id, then to_param', (TIMES * FACTOR).ceil do
-  #   ar { User.find(get_id).to_param }
-  # end
-  # #
-  # report 'find unslugged model using friendly id, then to_param', (TIMES * FACTOR).ceil do
-  #   ar { User.find(USERS.rand).to_param }
-  # end
-  # #
-  # report 'find slugged model using friendly id, then to_param', (TIMES * FACTOR).ceil do
-  #   ar { Post.find(POSTS.rand).to_param }
-  # end
-  # #
-  # report 'find cached slugged model using friendly id, then to_param', (TIMES * FACTOR).ceil do
-  #   ar { District.find(DISTRICTS.rand).to_param }
-  # end
+  report 'find model using id, then to_param', (TIMES * FACTOR).ceil do
+    ar { User.find(get_id).to_param }
+  end
+
+  report 'find unslugged model using friendly id, then to_param', (TIMES * FACTOR).ceil do
+    ar { User.find(USERS.rand).to_param }
+  end
+
+  report 'find slugged model using friendly id, then to_param', (TIMES * FACTOR).ceil do
+    ar { Post.find(POSTS.rand).to_param }
+  end
+
+  report 'find cached slugged model using friendly id, then to_param', (TIMES * FACTOR).ceil do
+    ar { District.find(DISTRICTS.rand).to_param }
+   end
 
   summary 'Total'
 end
--- extras/extras.rb
+++ extras/extras.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby -KU
 require File.dirname(__FILE__) + '/../test/test_helper'
+require File.dirname(__FILE__) + '/../test/active_record2/test_helper'
 require 'ffaker'
 
 TIMES = (ENV['N'] || 100).to_i
