c41bc997b546d2bb8ef0466fb011983f300351db
Nash Kabbara <nkabbara@vaio.(none)>
2010-03-10 14:27:37 -0600

    * Fixed regex to support multiple digit sequences
    The parse_friendly_id method was only grabbing the first number in a
    squence. Modified regex to support multiple digit numbers

--- test/friendly_id_test.rb
+++ test/friendly_id_test.rb
@@ -10,12 +10,28 @@ module FriendlyId
         assert_equal ["test", "2"], "test--2".parse_friendly_id
       end
 
+      test "should parse a friendly_id name and 10 as a sequence" do
+        assert_equal ["test", "10"], "test--10".parse_friendly_id
+      end
+
+      test "should parse a friendly_id name and 11 as a sequence" do
+        assert_equal ["test", "11"], "test--11".parse_friendly_id
+      end
+
+      test "should parse a friendly_id name and 29 as a sequence" do
+        assert_equal ["test", "29"], "test--29".parse_friendly_id
+      end
+
       test "should parse with a default sequence of 1" do
         assert_equal ["test", "1"], "test".parse_friendly_id
       end
 
       test "should be parseable with a custom separator" do
         assert_equal ["test", "2"], "test:2".parse_friendly_id(":")
+      end
+
+      test "should be parseable with a custom separator and a double digit sequence" do
+        assert_equal ["test", "12"], "test:12".parse_friendly_id(":")
       end
 
       test "should parse when default sequence separator occurs in friendly_id name" do
@@ -33,6 +49,11 @@ module FriendlyId
       test "should parse when sequence separator, number and sequence occur in friendly_id name" do
         assert_equal ["test--2--test", "2"], "test--2--test--2".parse_friendly_id
       end
+
+      test "should parse when double digit sequence separator, number and sequence occur in friendly_id name" do
+        assert_equal ["test--2--test", "12"], "test--2--test--12".parse_friendly_id
+      end
+
 
     end
   end
