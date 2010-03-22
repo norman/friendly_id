0f7d75cd7b47e8a73f20210ac7618ba307d5550e
Norman Clarke <norman@njclarke.com>
2010-03-22 11:44:54 -0300

    Made status tests generic.

--- lib/friendly_id/test.rb
+++ lib/friendly_id/test.rb
@@ -154,6 +154,72 @@ module FriendlyId
 
     end
 
+    # Tests for FriendlyId::Status.
+    module Status
+      test "should default to not friendly" do
+        assert !status.friendly?
+      end
+
+      test "should default to numeric" do
+        assert status.numeric?
+      end
+    end
+    
+    # Tests for FriendlyId::Status for a model that uses slugs.
+    module SluggedStatus
+
+      test "should be friendly if slug is set" do
+        status.slug = Slug.new
+        assert status.friendly?
+      end
+
+      test "should be friendly if name is set" do
+        status.name = "name"
+        assert status.friendly?
+      end
+
+      test "should be current if current slug is set" do
+        status.slug = instance.slug
+        assert status.current?
+      end
+
+      test "should not be current if non-current slug is set" do
+        status.slug = Slug.new(:sluggable => instance)
+        assert !status.current?
+      end
+
+      test "should be best if it is current" do
+        status.slug = instance.slug
+        assert status.best?
+      end
+
+      test "should be best if it is numeric, but record has no slug" do
+        instance.slugs = []
+        instance.slug = nil
+        assert status.best?
+      end
+
+      [:record, :name].each do |symbol|
+        test "should have #{symbol} after find using friendly_id" do
+          instance2 = klass.find(instance.friendly_id)
+          assert_not_nil instance2.friendly_id_status.send(symbol)
+        end
+      end
+
+      def status
+        @status ||= instance.friendly_id_status
+      end
+
+      def klass
+        raise NotImplementedError
+      end
+
+      def instance
+        raise NotImplementedError
+      end
+
+    end
+
     # Tests for models to ensure that they properly implement using the
     # +normalize_friendly_id+ method to allow developers to hook into the
     # slug string generation.
--- test/active_record2/slugged_status_test.rb
+++ test/active_record2/slugged_status_test.rb
@@ -7,51 +7,8 @@ module FriendlyId
 
       class StatusTest < ::Test::Unit::TestCase
 
-        test "should default to not friendly" do
-          assert !status.friendly?
-        end
-
-        test "should default to numeric" do
-          assert status.numeric?
-        end
-
-        test "should be friendly if slug is set" do
-          status.slug = Slug.new
-          assert status.friendly?
-        end
-
-        test "should be friendly if name is set" do
-          status.name = "name"
-          assert status.friendly?
-        end
-
-        test "should be current if current slug is set" do
-          status.slug = instance.slug
-          assert status.current?
-        end
-
-        test "should not be current if non-current slug is set" do
-          status.slug = Slug.new(:sluggable => instance)
-          assert !status.current?
-        end
-
-        test "should be best if it is current" do
-          status.slug = instance.slug
-          assert status.best?
-        end
-
-        test "should be best if it is numeric, but record has no slug" do
-          instance.slugs = []
-          instance.slug = nil
-          assert status.best?
-        end
-
-        [:record, :name].each do |symbol|
-          test "should have #{symbol} after find using friendly_id" do
-            instance2 = klass.find(instance.friendly_id)
-            assert_not_nil instance2.friendly_id_status.send(symbol)
-          end
-        end
+        include FriendlyId::Test::Status
+        include FriendlyId::Test::SluggedStatus
 
         def klass
           Post
@@ -59,10 +16,6 @@ module FriendlyId
 
         def instance
           @instance ||= klass.create! :name => "hello world"
-        end
-
-        def status
-          @status ||= instance.friendly_id_status
         end
 
       end
