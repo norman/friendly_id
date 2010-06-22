1bf664a91ea305888e59abb4bc3f17bcf8656e93
Norman Clarke <norman@njclarke.com>
2010-06-22 19:10:28 -0300

    Stop loading from .bundle for Bundler 1.0.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,14 +4,9 @@ $:.uniq!
 
 $KCODE = "UTF8" if RUBY_VERSION < "1.9"
 $VERBOSE = false
-begin
-  require File.expand_path('../../.bundle/environment', __FILE__)
-rescue LoadError
-  # Fall back on doing an unlocked resolve at runtime.
-  require "rubygems"
-  require "bundler"
-  Bundler.setup
-end
+require "rubygems"
+require "bundler"
+Bundler.setup
 require "test/unit"
 require "mocha"
 require "active_support"
