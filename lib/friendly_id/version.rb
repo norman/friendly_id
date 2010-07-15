a55ffb4fb170690c6b323422c38d0bdb0abca470
Norman Clarke <norman@njclarke.com>
2010-07-15 02:52:04 -0300

    Bump version.

--- lib/friendly_id/version.rb
+++ lib/friendly_id/version.rb
@@ -1,9 +1,9 @@
 module FriendlyId
   module Version
     MAJOR = 3
-    MINOR = 0
-    TINY = 6
-    BUILD = nil
+    MINOR = 1
+    TINY = 0
+    BUILD = "pre"
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
 end
