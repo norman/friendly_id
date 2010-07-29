e8a5eb859a049dbaabf40a7825e5131c82d512aa
Norman Clarke <norman@njclarke.com>
2010-07-29 11:29:43 -0300

    Small typo fix.

--- lib/friendly_id/datamapper.rb
+++ lib/friendly_id/datamapper.rb
@@ -1,5 +1,5 @@
 begin
   require 'friendly_id_datamapper'
 rescue LoadError
-  raise "To use FriendlyId's Sequel adapter, please `gem install friendly_id_datamapper`"
+  raise "To use FriendlyId's DataMapper adapter, please `gem install friendly_id_datamapper`"
 end
