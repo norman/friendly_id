3b4a063ece4fa426e78d1e9e746905d57b0547d1
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2010-12-26 17:48:28 +0100

    More friendly conditions for string IDs added

--- lib/friendly_id/active_record_adapter/tasks.rb
+++ lib/friendly_id/active_record_adapter/tasks.rb
@@ -28,13 +28,15 @@ module FriendlyId
       validate_uses_slugs
       cond = "slugs.id IS NULL"
       options = {:limit => 100, :include => :slugs, :conditions => cond, :order => "#{klass.table_name}.id ASC"}.merge(task_options || {})
+      offset = 0
       while records = find(:all, options) do
         break if records.size == 0
         records.each do |record|
           record.save(:validate => false)
           yield(record) if block_given?
         end
-        options[:conditions] = cond + " and #{klass.table_name}.id > #{records.last.id}"
+        offset+=options[:limit]
+        options[:offset]=|| offset
       end
     end
 
