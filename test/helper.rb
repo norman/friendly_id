ff9f4f5ded62acbdbfc26c38a1681b60e5f709b2
Norman Clarke <norman@njclarke.com>
2011-07-17 15:29:37 -0300

    Don't remove single quotes from test names

--- test/helper.rb
+++ test/helper.rb
@@ -79,7 +79,7 @@ end
 
 class Module
   def test(name, &block)
-    define_method("test_#{name.gsub(/[^a-z0-9]/i, "_")}".to_sym, &block)
+    define_method("test_#{name.gsub(/[^a-z0-9']/i, "_")}".to_sym, &block)
   end
 end
 
