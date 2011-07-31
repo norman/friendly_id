ace612152097c88321da96f6cf261e83d3bf80f3
Norman Clarke <norman@njclarke.com>
2011-07-31 20:14:58 -0300

    More documentation

--- lib/friendly_id.rb
+++ lib/friendly_id.rb
@@ -51,6 +51,214 @@ module FriendlyId
   end
 
   # Set global defaults for all models using FriendlyId.
+  #
+  # @example
+  #   FriendlyId.defaults do |config|
+  #     config.base = :name
+  #     config.use :slugged
+  #   end
+  def self.defaults(&block)
+    @mutex.synchronize do
+      @defaults = block if block_given?
+    end
+    @defaults
+  end
+
+end
+require "thread"
+require "friendly_id/base"
+require "friendly_id/model"
+require "friendly_id/object_utils"
+require "friendly_id/configuration"
+require "friendly_id/finder_methods"
+
+# FriendlyId is a comprehensive Ruby library for ActiveRecord permalinks and
+# slugs.
+#
+# @author Norman Clarke
+module FriendlyId
+
+  @mutex = Mutex.new
+
+  autoload :Slugged,  "friendly_id/slugged"
+  autoload :Scoped,   "friendly_id/scoped"
+  autoload :History,  "friendly_id/history"
+
+  # FriendlyId takes advantage of `extended` to do basic model setup, primarily
+  # extending {FriendlyId::Base} to add {FriendlyId::Base#friendly_id
+  # friendly_id} as a class method.
+  #
+  # Previous versions of FriendlyId simply patched ActiveRecord::Base, but this
+  # version tries to be less invasive.
+  #
+  # In addition to adding {FriendlyId::Base.friendly_id friendly_id}, the class
+  # instance variable +@friendly_id_config+ is added. This variable is an
+  # instance of an anonymous subclass of {FriendlyId::Configuration}. This
+  # allows subsequently loaded modules like {FriendlyId::Slugged} and
+  # {FriendlyId::Scoped} to add functionality to the configuration class only
+  # for the current class, rather than monkey patching
+  # {FriendlyId::Configuration} directly. This isolates other models from large
+  # feature changes an addon to FriendlyId could potentially introduce.
+  #
+  # The upshot of this is, you can htwo Active Record models that both have a
+  # @friendly_id_config, but each config object can have different methods and
+  # behaviors depending on what modules have been loaded, without conflicts.
+  # Keep this in mind if you're hacking on FriendlyId.
+  #
+  # For examples of this, see the source for {Scoped.included}.
+  def self.extended(base)
+    base.instance_eval do
+      extend FriendlyId::Base
+      @friendly_id_config = Class.new(FriendlyId::Configuration).new(base)
+      if defaults = FriendlyId.defaults
+        defaults.yield @friendly_id_config
+      end
+    end
+    ActiveRecord::Relation.send :include, FriendlyId::FinderMethods
+  end
+
+  # Set global defaults for all models using FriendlyId.
+  #
+  # @example
+  #   FriendlyId.defaults do |config|
+  #     config.base = :name
+  #     config.use :slugged
+  #   end
+  def self.defaults(&block)
+    @mutex.synchronize do
+      @defaults = block if block_given?
+    end
+    @defaults
+  end
+
+end
+require "thread"
+require "friendly_id/base"
+require "friendly_id/model"
+require "friendly_id/object_utils"
+require "friendly_id/configuration"
+require "friendly_id/finder_methods"
+
+# FriendlyId is a comprehensive Ruby library for ActiveRecord permalinks and
+# slugs.
+#
+# @author Norman Clarke
+module FriendlyId
+
+  @mutex = Mutex.new
+
+  autoload :Slugged,  "friendly_id/slugged"
+  autoload :Scoped,   "friendly_id/scoped"
+  autoload :History,  "friendly_id/history"
+
+  # FriendlyId takes advantage of `extended` to do basic model setup, primarily
+  # extending {FriendlyId::Base} to add {FriendlyId::Base#friendly_id
+  # friendly_id} as a class method.
+  #
+  # Previous versions of FriendlyId simply patched ActiveRecord::Base, but this
+  # version tries to be less invasive.
+  #
+  # In addition to adding {FriendlyId::Base.friendly_id friendly_id}, the class
+  # instance variable +@friendly_id_config+ is added. This variable is an
+  # instance of an anonymous subclass of {FriendlyId::Configuration}. This
+  # allows subsequently loaded modules like {FriendlyId::Slugged} and
+  # {FriendlyId::Scoped} to add functionality to the configuration class only
+  # for the current class, rather than monkey patching
+  # {FriendlyId::Configuration} directly. This isolates other models from large
+  # feature changes an addon to FriendlyId could potentially introduce.
+  #
+  # The upshot of this is, you can htwo Active Record models that both have a
+  # @friendly_id_config, but each config object can have different methods and
+  # behaviors depending on what modules have been loaded, without conflicts.
+  # Keep this in mind if you're hacking on FriendlyId.
+  #
+  # For examples of this, see the source for {Scoped.included}.
+  def self.extended(base)
+    base.instance_eval do
+      extend FriendlyId::Base
+      @friendly_id_config = Class.new(FriendlyId::Configuration).new(base)
+      if defaults = FriendlyId.defaults
+        defaults.yield @friendly_id_config
+      end
+    end
+    ActiveRecord::Relation.send :include, FriendlyId::FinderMethods
+  end
+
+  # Set global defaults for all models using FriendlyId.
+  #
+  # @example
+  #   FriendlyId.defaults do |config|
+  #     config.base = :name
+  #     config.use :slugged
+  #   end
+  def self.defaults(&block)
+    @mutex.synchronize do
+      @defaults = block if block_given?
+    end
+    @defaults
+  end
+
+end
+require "thread"
+require "friendly_id/base"
+require "friendly_id/model"
+require "friendly_id/object_utils"
+require "friendly_id/configuration"
+require "friendly_id/finder_methods"
+
+# FriendlyId is a comprehensive Ruby library for ActiveRecord permalinks and
+# slugs.
+#
+# @author Norman Clarke
+module FriendlyId
+
+  @mutex = Mutex.new
+
+  autoload :Slugged,  "friendly_id/slugged"
+  autoload :Scoped,   "friendly_id/scoped"
+  autoload :History,  "friendly_id/history"
+
+  # FriendlyId takes advantage of `extended` to do basic model setup, primarily
+  # extending {FriendlyId::Base} to add {FriendlyId::Base#friendly_id
+  # friendly_id} as a class method.
+  #
+  # Previous versions of FriendlyId simply patched ActiveRecord::Base, but this
+  # version tries to be less invasive.
+  #
+  # In addition to adding {FriendlyId::Base.friendly_id friendly_id}, the class
+  # instance variable +@friendly_id_config+ is added. This variable is an
+  # instance of an anonymous subclass of {FriendlyId::Configuration}. This
+  # allows subsequently loaded modules like {FriendlyId::Slugged} and
+  # {FriendlyId::Scoped} to add functionality to the configuration class only
+  # for the current class, rather than monkey patching
+  # {FriendlyId::Configuration} directly. This isolates other models from large
+  # feature changes an addon to FriendlyId could potentially introduce.
+  #
+  # The upshot of this is, you can htwo Active Record models that both have a
+  # @friendly_id_config, but each config object can have different methods and
+  # behaviors depending on what modules have been loaded, without conflicts.
+  # Keep this in mind if you're hacking on FriendlyId.
+  #
+  # For examples of this, see the source for {Scoped.included}.
+  def self.extended(base)
+    base.instance_eval do
+      extend FriendlyId::Base
+      @friendly_id_config = Class.new(FriendlyId::Configuration).new(base)
+      if defaults = FriendlyId.defaults
+        defaults.yield @friendly_id_config
+      end
+    end
+    ActiveRecord::Relation.send :include, FriendlyId::FinderMethods
+  end
+
+  # Set global defaults for all models using FriendlyId.
+  #
+  # @example
+  #
+  #   FriendlyId.defaults do |config|
+  #     config.base = :name
+  #     config.use :slugged
+  #   end
   def self.defaults(&block)
     @mutex.synchronize do
       @defaults = block if block_given?
--- lib/friendly_id/base.rb
+++ lib/friendly_id/base.rb
@@ -2,27 +2,72 @@ module FriendlyId
   # Class methods that will be added to ActiveRecord::Base.
   module Base
 
-    # Configure FriendlyId for a model. Use this method to configure FriendlyId
-    # for your model.
+    # Configure FriendlyId's behavior in a model.
     #
     #   class Post < ActiveRecord::Base
     #     extend FriendlyId
     #     friendly_id :title, :use => :slugged
     #   end
     #
+    # When given the optional block, this method will yield the class's instance
+    # of {FriendlyId::Configuration} to the block before evaluating other
+    # arguments, so configuration values set in the block may be overwritten by
+    # the arguments. This order was chosen to allow passing the same proc to
+    # multiple models, while being able to override the values it sets. Here
+    # is a contrived example:
     #
-    # When a block is given, this method will yield to block before evaluating
-    # other arguments, so values set in the block will be overwritten by
-    # arguments. This can be useful when reusing a proc to set defaults in
-    # multiple models. However, in most cases setting global defaults can be
-    # better accomplished by using {FriendlyId.defaults FriendlyId.defaults}.
+    #   $friendly_id_config_proc = Proc.new do |config|
+    #     config.base = :name
+    #     config.use :slugged
+    #   end
+    #
+    #   class Foo < ActiveRecord::Base
+    #     extend FriendlyId
+    #     friendly_id &$friendly_id_config_proc
+    #   end
+    #
+    #   class Bar < ActiveRecord::Base
+    #     extend FriendlyId
+    #     friendly_id :title, &$friendly_id_config_proc
+    #   end
+    #
+    # However, it's usually better to use {FriendlyId.defaults} for this:
+    #
+    #   FriendlyId.defaults do |config|
+    #     config.base = :name
+    #     config.use :slugged
+    #   end
+    #
+    #   class Foo < ActiveRecord::Base
+    #     extend FriendlyId
+    #   end
+    #
+    #   class Bar < ActiveRecord::Base
+    #     extend FriendlyId
+    #     friendly_id :title
+    #   end
+    #
+    # In general you should use the block syntax either because of your personal
+    # aesthetic preference, or because you need to share some functionality
+    # between multiple models that can't be well encapsulated by
+    # {FriendlyId.defaults}.
     #
     # @option options [Symbol] :use The name of an addon to use. By default,
     #   FriendlyId provides {FriendlyId::Slugged :slugged},
     #   {FriendlyId::History :history} and {FriendlyId::Scoped :scoped}.
+    # @option options [Symbol] :scope Available when using +:scoped+.
+    #   Sets the relation or column used to scope generated friendly ids. This
+    #   option has no default value.
+    # @option options [Symbol] :sequence_separator Available when using +:slugged+.
+    #   Configures the sequence of characters used to separate a slug from a
+    #   sequence. Defaults to +--+.
     # @option options [Symbol] :slug_column Available when using +:slugged+.
     #   Configures the name of the column where FriendlyId will store the slug.
     #   Defaults to +:slug+.
+    # @option options [Symbol] :slug_sequencer_class Available when using +:slugged+.
+    #   Sets the class used to generate unique slugs. You should not specify this
+    #   unless you're doing some extensive hacking on FriendlyId. Defaults to
+    #   {FriendlyId::SlugSequencer}.
     def friendly_id(base = nil, options = {}, &block)
       yield @friendly_id_config if block_given?
       @friendly_id_config.use options.delete :use
