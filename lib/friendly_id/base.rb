ca350c6926e42964e2ec4647fafca16d1acb9a44
Norman Clarke <norman@njclarke.com>
2011-08-08 23:44:44 -0300

    Documented required order of calls to #use

--- lib/friendly_id/base.rb
+++ lib/friendly_id/base.rb
@@ -13,8 +13,8 @@ module FriendlyId
     # of {FriendlyId::Configuration} to the block before evaluating other
     # arguments, so configuration values set in the block may be overwritten by
     # the arguments. This order was chosen to allow passing the same proc to
-    # multiple models, while being able to override the values it sets. Here
-    # is a contrived example:
+    # multiple models, while being able to override the values it sets. Here is
+    # a contrived example:
     #
     #   $friendly_id_config_proc = Proc.new do |config|
     #     config.base = :name
@@ -51,6 +51,33 @@ module FriendlyId
     # aesthetic preference, or because you need to share some functionality
     # between multiple models that can't be well encapsulated by
     # {FriendlyId.defaults}.
+    #
+    # === Order Method Calls in a Block vs Ordering Options
+    #
+    # When calling this method without a block, you may set the hash options in
+    # any order.
+    #
+    # However, when using block-style invocation, be sure to call
+    # FriendlyId::Configuration's {FriendlyId::Configuration#use use} method
+    # *prior* to the associated configuration options, because it will include
+    # modules into your class, and these modules in turn may add required
+    # configuration options to the +@friendly_id_configuraton+'s class:
+    #
+    #   class Person < ActiveRecord::Base
+    #     friendly_id do |config|
+    #       # This will work
+    #       config.use :slugged
+    #       config.sequence_separator = ":"
+    #     end
+    #   end
+    #
+    #   class Person < ActiveRecord::Base
+    #     friendly_id do |config|
+    #       # This will fail
+    #       config.sequence_separator = ":"
+    #       config.use :slugged
+    #     end
+    #   end
     #
     # @option options [Symbol] :use The name of an addon to use. By default,
     #   FriendlyId provides {FriendlyId::Slugged :slugged},
@@ -94,6 +121,10 @@ module FriendlyId
     end
 
     # Returns the model class's {FriendlyId::Configuration friendly_id_config}.
+    # @note In the case of Single Table Inheritance (STI), this method will
+    #   duplicate the parent class's FriendlyId::Configuration instance on first
+    #   access. If you're concerned about thread safety, then be sure to invoke
+    #   {#friendly_id} in your class for each model.
     def friendly_id_config
       @friendly_id_config
     end
