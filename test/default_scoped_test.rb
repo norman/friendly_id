0c6519ac49053014c0db78bd0cd465c5c80b6e18
Benoit Bénézech <benoit.benezech@gmail.com>
2011-08-30 15:41:46 +0300

    Sent from my Iphone

--- test/default_scoped_test.rb
+++ test/default_scoped_test.rb
@@ -1,5 +1,3 @@
-require File.expand_path("../helper.rb", __FILE__)
-
 class DefaultScopeTest < MiniTest::Unit::TestCase
 
   include FriendlyId::Test
@@ -11,14 +9,23 @@ class DefaultScopeTest < MiniTest::Unit::TestCase
   end
   
   test "friendly_id should sequence correctly a default_scoped ordered table" do
+    OrderedJournalist.delete_all
     OrderedJournalist.create!({ :name => 'I\'m unique', :position => 1, :department => 'main_service' })
     OrderedJournalist.create!({ :name => 'I\'m unique', :position => 2, :department => 'main_service' })
-    OrderedJournalist.create!({ :name => 'I\'m unique', :position => 3, :department => 'main_service' }) # should not raise ActiveRecord::RecordNotUnique: SQLite3::ConstraintException: column slug is not unique: INSERT INTO "ordered_journalists" ("name", "active", "slug", "position") VALUES ('I''m unique', NULL, 'i-m-unique--2', 3)
+    begin
+      OrderedJournalist.create!({ :name => 'I\'m unique', :position => 3, :department => 'main_service' }) # should not raise ActiveRecord::RecordNotUnique: SQLite3::ConstraintException: column slug is not unique: INSERT INTO "ordered_journalists" ("name", "active", "slug", "position") VALUES ('I''m unique', NULL, 'i-m-unique--2', 3)
+    rescue
+      flunk "expected no errors but got #{$!}"
+    end
   end
   
   test "friendly_id should sequence correctly a default_scoped scoped table" do
+    OrderedJournalist.delete_all
     OrderedJournalist.create!({ :name => 'I\'m unique', :department => 'other_service' })
-    OrderedJournalist.create!({ :name => 'I\'m unique', :department => 'main_service' }) # should not raise ActiveRecord::RecordNotUnique: SQLite3::ConstraintException: column slug is not unique: INSERT INTO "ordered_journalists" ("name", "active", "slug", "position", "department") VALUES ('I''m unique', NULL, 'i-m-unique', 1, 'main_service')
+    begin
+      OrderedJournalist.create!({ :name => 'I\'m unique', :department => 'main_service' }) # should not raise ActiveRecord::RecordNotUnique: SQLite3::ConstraintException: column slug is not unique: INSERT INTO "ordered_journalists" ("name", "active", "slug", "position", "department") VALUES ('I''m unique', NULL, 'i-m-unique', 1, 'main_service')
+    rescue
+      flunk "expected no errors but got #{$!}"
+    end
   end
 end
-
