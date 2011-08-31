bb8afc4765f973b66bea7d558542834a509e8745
Norman Clarke <norman@njclarke.com>
2011-08-31 16:11:15 -0300

    Bumped version.
    [ci skip]

--- lib/friendly_id.rb
+++ lib/friendly_id.rb
@@ -81,7 +81,7 @@ In general, use slugs by default unless you know for sure you don't need them.
 module FriendlyId
 
   # The current version.
-  VERSION = "4.0.0.beta9"
+  VERSION = "4.0.0.beta10"
 
   @mutex = Mutex.new
 
