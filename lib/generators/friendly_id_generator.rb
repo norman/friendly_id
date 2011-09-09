c03879ac0227fab4a840b22c848552464b513591
Norman Clarke <norman@njclarke.com>
2011-09-08 22:33:35 -0300

    Use Rails-provided module for migration numbering

--- lib/generators/friendly_id_generator.rb
+++ lib/generators/friendly_id_generator.rb
@@ -1,10 +1,11 @@
 require 'rails/generators'
-require 'rails/generators/migration'
+require "rails/generators/active_record"
 
 # This generator adds a migration for the {FriendlyId::History
 # FriendlyId::History} addon.
 class FriendlyIdGenerator < Rails::Generators::Base
   include Rails::Generators::Migration
+  extend ActiveRecord::Generators::Migration
 
   source_root File.expand_path('../../friendly_id', __FILE__)
 
@@ -13,14 +14,4 @@ class FriendlyIdGenerator < Rails::Generators::Base
     migration_template 'migration.rb', 'db/migrate/create_friendly_id_slugs.rb'
   end
 
-  # TODO: use the module provided with Rails, no need to do this
-  # any more
-  # Taken from ActiveRecord's migration generator
-  def self.next_migration_number(dirname) #:nodoc:
-    if ActiveRecord::Base.timestamped_migrations
-      Time.now.utc.strftime("%Y%m%d%H%M%S")
-    else
-      "%.3d" % (current_migration_number(dirname) + 1)
-    end
-  end
 end
