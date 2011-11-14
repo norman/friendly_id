471d2b70aabc5e0f0cc3291d7b417c9e2afc458c
Norman Clarke <norman@njclarke.com>
2011-11-13 23:02:09 -0300

    Added (failing) threading test
    This tests the concurrency issue described in GH #180.

---
+++ test/compatibility/threading/threading.rb
@@ -0,0 +1,42 @@
+ENV["DB"] = "postgres"
+
+require "thread"
+require File.expand_path("../../../helper", __FILE__)
+
+ActiveRecord::Migration.tap do |m|
+  m.drop_table "things"
+  m.create_table("things") do |t|
+    t.string  :name
+    t.string  :slug
+  end
+  m.add_index :things, :slug, :unique => true
+end
+
+class Thing < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :name, :use => :slugged
+end
+
+$things = 10.times.map do
+  Thing.new :name => "a b c"
+end
+
+$mutex = Mutex.new
+
+def save_thing
+  thing = $mutex.synchronize do
+    $things.pop
+  end
+  if thing.nil? then return end
+  value = thing.save!
+  print "#{thing.friendly_id}\n"
+  true
+end
+
+2.times.map do
+  Thread.new do
+    while true do
+      break unless save_thing
+    end
+  end
+end.map(&:value)
