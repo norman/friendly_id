62c0341d93d577c8f0895717cbcbbe34e60036c0
Norman Clarke <norman@njclarke.com>
2011-12-20 11:55:07 -0300

    Added Gemfile for testing on Rails 3.2.

---
+++ gemfiles/Gemfile.rails-3.2.rb
@@ -0,0 +1,21 @@
+source :rubygems
+
+# platform :jruby do
+#   gem "activerecord-jdbc-adapter", :git => "https://github.com/nicksieger/activerecord-jdbc-adapter.git"
+#   gem "activerecord-jdbcmysql-adapter"
+#   gem "activerecord-jdbcpostgresql-adapter"
+#   gem "activerecord-jdbcsqlite3-adapter"
+# end
+
+platform :ruby do
+  gem "mysql2", "~> 0.3.6"
+  gem "pg", "~> 0.11.0"
+  gem "sqlite3", "~> 1.3.4"
+end
+
+gem "ffaker"
+gem "activerecord", "~> 3.2.0.rc1"
+gem "minitest", "~> 2.4.0"
+gem "mocha", "~> 0.9.12"
+gem "railties", "~> 3.2.0.rc1"
+gem "rake"
