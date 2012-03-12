8a5ee980e235b94015fd3f493710984bd5920fec
Sergey Petrunin <SPetrunin@sphereconsultinginc.com>
2012-03-12 20:04:40 +0200

    add test for case when friendly_id used only in childs of sti model

---
+++ test/sti_test_for_childs.rb
@@ -0,0 +1,59 @@
+require File.expand_path("../helper.rb", __FILE__)
+
+class StiTest < MiniTest::Unit::TestCase
+
+  include FriendlyId::Test
+  include FriendlyId::Test::Shared::Core
+  include FriendlyId::Test::Shared::Slugged
+
+  class Journalist < ActiveRecord::Base
+  end
+
+  class Editorialist < Journalist
+    extend FriendlyId
+    friendly_id :name, :use => :slugged
+  end
+
+  def model_class
+    Editorialist
+  end
+
+  test "friendly_id should accept a base and a hash with single table inheritance" do
+    abstract_klass = Class.new(ActiveRecord::Base) do
+      def self.table_exists?; false end
+      extend FriendlyId
+      friendly_id :foo, :use => :slugged, :slug_column => :bar
+    end
+    klass = Class.new(abstract_klass)
+    assert klass < FriendlyId::Slugged
+    assert_equal :foo, klass.friendly_id_config.base
+    assert_equal :bar, klass.friendly_id_config.slug_column
+  end
+
+  test "friendly_id should accept a block with single table inheritance" do
+    abstract_klass = Class.new(ActiveRecord::Base) do
+      def self.table_exists?; false end
+      extend FriendlyId
+      friendly_id :foo do |config|
+        config.use :slugged
+        config.base = :foo
+        config.slug_column = :bar
+      end
+    end
+    klass = Class.new(abstract_klass)
+    assert klass < FriendlyId::Slugged
+    assert_equal :foo, klass.friendly_id_config.base
+    assert_equal :bar, klass.friendly_id_config.slug_column
+  end
+
+  test "friendly_id slugs should not clash with eachother" do
+    journalist  = Journalist.create! :name => 'foo bar'
+    editoralist = Editorialist.create! :name => 'foo bar'
+    editoralist2 = Editorialist.create! :name => 'foo bar'
+
+    assert_equal 'foo-bar', journalist.slug
+    assert_equal 'foo-bar--2', editoralist.slug
+    assert_equal 'foo-bar--3', editoralist2.slug
+  end
+
+end
