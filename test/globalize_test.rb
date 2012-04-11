6a7960b9b53c2c9141a71cd21ef07985e69241d5
Chris Salzberg <chrissalzberg@gmail.com>
2012-04-11 11:32:19 +0900

    refactor and add test (for translated slugs using globalize).

--- test/globalize_test.rb
+++ test/globalize_test.rb
@@ -9,15 +9,32 @@ class TranslatedArticle < ActiveRecord::Base
 end
 
 class GlobalizeTest < MiniTest::Unit::TestCase
+  include FriendlyId::Test
+
+  test "friendly_id should find slug in current locale if locale is set, otherwise in default locale" do
+    transaction do
+      I18n.default_locale = :en
+      article_en = I18n.with_locale(:en) { TranslatedArticle.create(:title => 'a title') }
+      article_de = I18n.with_locale(:de) { TranslatedArticle.create(:title => 'titel') }
+
+      I18n.with_locale(:de) {
+        assert_equal TranslatedArticle.find("titel"), article_de
+        assert_equal TranslatedArticle.find("a-title"), article_en
+      }
+    end
+  end
 
   # https://github.com/svenfuchs/globalize3/blob/master/test/globalize3/dynamic_finders_test.rb#L101
   # see: https://github.com/svenfuchs/globalize3/issues/100
   test "record returned by friendly_id should have all translations" do
-    I18n.locale = :en
-    article = TranslatedArticle.create(:title => 'a title')
-    Globalize.with_locale(:ja) { article.update_attributes(:title => 'タイトル') }
-    article_by_friendly_id = TranslatedArticle.find("a-title")
-    assert_equal article.translations, article_by_friendly_id.translations
+    transaction do
+      I18n.with_locale(:en) do
+        article = TranslatedArticle.create(:title => 'a title')
+        Globalize.with_locale(:ja) { article.update_attributes(:title => 'タイトル') }
+        article_by_friendly_id = TranslatedArticle.find("a-title")
+        assert_equal article.translations, article_by_friendly_id.translations
+      end
+    end
   end
 
 end
