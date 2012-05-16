704286e9d9707d08e222f767da4c16bdb43471ad
nolamesa <diamanto87@gmail.com>
2012-05-16 17:38:55 +0300

    If there is no error in friendly_id attribute then #move_friendly_id_error_to_name removes any existing error from the name attribute.

--- lib/friendly_id/reserved.rb
+++ lib/friendly_id/reserved.rb
@@ -30,7 +30,7 @@ For example:
     after_validation :move_friendly_id_error_to_name
 
     def move_friendly_id_error_to_name
-      errors.messages[:name] = errors.messages.delete(:friendly_id)
+      errors.messages[:name] = errors.messages.delete(:friendly_id) if errors[:friendly_id].present?
     end
   end
 
