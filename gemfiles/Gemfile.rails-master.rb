759c50c080acec5bed84bb7e101b51d939366cea
Norman Clarke <norman@njclarke.com>
2012-05-31 10:36:57 -0300

    Add Rails 3-2-stable/master to build matrix
    Also exlude some other combinations to avoid too huge a matrix.

---
+++ gemfiles/Gemfile.rails-master.rb
@@ -0,0 +1,25 @@
+source :rubygems
+
+platforms :jruby do
+  gem 'activerecord-jdbcsqlite3-adapter'
+  gem 'activerecord-jdbcmysql-adapter'
+  gem 'activerecord-jdbcpostgresql-adapter'
+  gem 'jruby-openssl'
+end
+
+platforms :ruby do
+  gem 'sqlite3'
+  gem 'mysql2'
+  gem 'pg'
+end
+
+git "git://github.com/rails/rails.git" do
+  gem 'activerecord'
+  gem 'railties'
+end
+
+gem 'ffaker'
+gem 'minitest'
+gem 'mocha'
+gem 'rake'
+gem 'globalize3'
