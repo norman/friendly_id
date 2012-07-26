56f0059d147fcba569f087b8d0def9a6c58de78c
Philip Arndt <parndt@gmail.com>
2012-07-26 16:15:17 +1200

    Started working toward Rails4 compatibility.

---
+++ gemfiles/Gemfile.rails-4.0.rb
@@ -0,0 +1,36 @@
+source :rubygems
+
+platforms :jruby do
+  gem 'activerecord-jdbcsqlite3-adapter'
+  gem 'activerecord-jdbcmysql-adapter'
+  gem 'activerecord-jdbcpostgresql-adapter'
+  gem 'jruby-openssl'
+end
+
+platforms :ruby do
+  gem 'sqlite3'
+  gem 'mysql2'
+  gem 'pg'
+end
+
+gem 'ffaker'
+git 'git://github.com/rails/rails.git' do
+  gem 'activerecord'
+  gem 'railties'
+end
+if ENV['JOURNEY']
+  gem 'journey', :path => ENV['JOURNEY']
+else
+  gem 'journey', :git => "git://github.com/rails/journey.git"
+end
+
+if ENV['AR_DEPRECATED_FINDERS']
+  gem 'active_record_deprecated_finders', :path => ENV['AR_DEPRECATED_FINDERS']
+else
+  gem 'active_record_deprecated_finders', :git => 'git://github.com/rails/active_record_deprecated_finders.git'
+end
+gem 'minitest'
+gem 'mocha'
+gem 'rake'
+gem 'globalize3', :git => 'git://github.com/svenfuchs/globalize3.git'
+gem 'paper_trail', :git => 'git://github.com/parndt/paper_trail.git', :branch => 'rails4'
