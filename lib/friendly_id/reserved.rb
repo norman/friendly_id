e275950e51588eb46c196b5baa423b0c7c33010b
Bruno Bonamin <bruno@bonamin.org>
2013-08-23 09:48:12 -0300

    Fix typo and formatting of Guide in correct files

--- lib/friendly_id/reserved.rb
+++ lib/friendly_id/reserved.rb
@@ -4,7 +4,7 @@ module FriendlyId
 
 ## Reserved Words
 
-The {FriendlyId::Reserved Reserved} module adds the ability to exlude a list of
+The {FriendlyId::Reserved Reserved} module adds the ability to exclude a list of
 words from use as FriendlyId slugs.
 
 With Ruby on Rails, FriendlyId's generator generates an initializer that
@@ -17,16 +17,16 @@ it will have no field to highlight. If you'd like to change this so that
 scaffolding works as expected, one way to accomplish this is to move the error
 message to a different field. For example:
 
-  class Person < ActiveRecord::Base
-    extend FriendlyId
-    friendly_id :name, use: :slugged
+    class Person < ActiveRecord::Base
+      extend FriendlyId
+      friendly_id :name, use: :slugged
 
-    after_validation :move_friendly_id_error_to_name
+      after_validation :move_friendly_id_error_to_name
 
-    def move_friendly_id_error_to_name
-      errors.add :name, *errors.delete(:friendly_id) if errors[:friendly_id].present?
+      def move_friendly_id_error_to_name
+        errors.add :name, *errors.delete(:friendly_id) if errors[:friendly_id].present?
+      end
     end
-  end
 
 =end
   module Reserved
