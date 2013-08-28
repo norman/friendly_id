684c43ea161fa0f43c8c95e75cff1cf437ba51c7
Philip Arndt <p@arndt.io>
2013-08-29 08:50:31 +1200

    Revert "Merge pull request #444 from gaganawhad/syntax-change-for-migration-methods"
    This reverts commit d7255049aa73ce7ba333ce814a36be430228fab0, reversing
    changes made to 3ce9e3c3fc3d3db9015e8e6c39afca8ac5709707.

--- lib/friendly_id/slugged.rb
+++ lib/friendly_id/slugged.rb
@@ -47,7 +47,7 @@ app's behavior and requirements.
 
     # a migration
     class CreatePosts < ActiveRecord::Migration
-      def up
+      def self.up
         create_table :posts do |t|
           t.string :title, :null => false
           t.string :slug, :null => false
@@ -57,7 +57,7 @@ app's behavior and requirements.
         add_index :posts, :slug, :unique => true
       end
 
-      def down
+      def self.down
         drop_table :posts
       end
     end
