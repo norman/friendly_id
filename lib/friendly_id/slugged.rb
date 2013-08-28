11665b1207339cb3d4b830eac109d73f76285955
Gagan Awhad <gaganaawhad@gmail.com>
2013-08-28 15:34:41 -0500

    Using newer instance method rails syntax for migration methods

--- lib/friendly_id/slugged.rb
+++ lib/friendly_id/slugged.rb
@@ -47,7 +47,7 @@ app's behavior and requirements.
 
     # a migration
     class CreatePosts < ActiveRecord::Migration
-      def self.up
+      def up
         create_table :posts do |t|
           t.string :title, :null => false
           t.string :slug, :null => false
@@ -57,7 +57,7 @@ app's behavior and requirements.
         add_index :posts, :slug, :unique => true
       end
 
-      def self.down
+      def down
         drop_table :posts
       end
     end
