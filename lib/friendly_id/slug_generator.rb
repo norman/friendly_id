0e20378fae4687846a505046bfc46a09e1fee2a7
Adrian Mugnolo <adrian@wyeworks.com>
2014-06-12 23:53:21 -0300

    Remove unused method

--- lib/friendly_id/slug_generator.rb
+++ lib/friendly_id/slug_generator.rb
@@ -1,6 +1,6 @@
 module FriendlyId
   # The default slug generator offers functionality to check slug strings for
-  # uniqueness and, if necessary, appends a sequence to guarantee it.
+  # uniqueness.
   class SlugGenerator
 
     def initialize(scope)
@@ -11,12 +11,8 @@ module FriendlyId
       !@scope.exists_by_friendly_id?(slug)
     end
 
-    def add(slug)
-      slug
-    end
-
     def generate(candidates)
-      candidates.each {|c| return add c if available?(c)}
+      candidates.each {|c| return c if available?(c)}
       nil
     end
 
