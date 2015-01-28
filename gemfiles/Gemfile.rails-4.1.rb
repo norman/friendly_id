22b66f2a40e95ec39e7000f5ebfee050017e1cba
Norman Clarke <norman@njclarke.com>
2015-01-28 10:28:19 -0800

    Update CI and gems

--- gemfiles/Gemfile.rails-4.1.rb
+++ gemfiles/Gemfile.rails-4.1.rb
@@ -2,15 +2,15 @@ source 'https://rubygems.org'
 
 gemspec path: '../'
 
-gem 'activerecord', '~> 4.1.0'
-gem 'railties', '~> 4.1.0'
+gem 'activerecord', '~> 4.1.9'
+gem 'railties', '~> 4.1.9'
 
 # Database Configuration
 group :development, :test do
   platforms :jruby do
-    gem 'activerecord-jdbcsqlite3-adapter', '>= 1.3.0.beta2'
-    gem 'activerecord-jdbcmysql-adapter', '>= 1.3.0.beta2'
-    gem 'activerecord-jdbcpostgresql-adapter', '>= 1.3.0.beta2'
+    gem 'activerecord-jdbcsqlite3-adapter', '~> 1.3.14'
+    gem 'activerecord-jdbcmysql-adapter', '~> 1.3.14'
+    gem 'activerecord-jdbcpostgresql-adapter', '~> 1.3.14'
     gem 'kramdown'
   end
 
--- gemfiles/Gemfile.rails-4.2.rb
+++ gemfiles/Gemfile.rails-4.2.rb
@@ -2,19 +2,15 @@ source 'https://rubygems.org'
 
 gemspec path: '../'
 
-gem 'rails', '~> 4.2.0.rc3' do
-  gem 'activerecord'
-  gem 'railties'
-end
-
-gem 'i18n', '0.7.0.beta1'
+gem 'activerecord', '~> 4.2.0'
+gem 'railties', '~> 4.2.0'
+gem 'i18n', '~> 0.7.0'
 
 # Database Configuration
 group :development, :test do
   platforms :jruby do
-    gem 'activerecord-jdbcsqlite3-adapter', '>= 1.3.0.beta2'
-    gem 'activerecord-jdbcmysql-adapter', '>= 1.3.0.beta2'
-    gem 'activerecord-jdbcpostgresql-adapter', '>= 1.3.0.beta2'
+    gem 'activerecord-jdbcmysql-adapter', '~> 1.3.14'
+    gem 'activerecord-jdbcpostgresql-adapter', '~> 1.3.14'
     gem 'kramdown'
   end
 
