0c3f70f528bfcae25dc7084a5b71123e5470ad8a
Michael Tucker <mtucker@godaddy.com>
2015-04-19 08:28:50 -0700

    change sort to use ID, since it is guaranteed unique where created_at is not and we are looking for a single unique record

--- lib/friendly_id/history.rb
+++ lib/friendly_id/history.rb
@@ -95,7 +95,7 @@ method.
       end
 
       def slug_table_record(id)
-        select(quoted_table_name + '.*').joins(:slugs).where(slug_history_clause(id)).order(Slug.arel_table[:created_at].desc).first
+        select(quoted_table_name + '.*').joins(:slugs).where(slug_history_clause(id)).order(Slug.arel_table[:id].desc).first
       end
 
       def slug_history_clause(id)
