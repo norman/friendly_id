e9a7ac14630d8bbc0026b75b05deb27898b9c42c
David Yun <abookyun@Davids-MacBook-Pro.local>
2015-09-22 21:43:22 +0800

    Make maximum use of 767 bytes

--- lib/friendly_id/migration.rb
+++ lib/friendly_id/migration.rb
@@ -8,8 +8,8 @@ class CreateFriendlyIdSlugs < ActiveRecord::Migration
       t.datetime :created_at
     end
     add_index :friendly_id_slugs, :sluggable_id
-    add_index :friendly_id_slugs, [:slug, :sluggable_type], length: { name: 100, slug: 20, sluggable_type: 20 }
-    add_index :friendly_id_slugs, [:slug, :sluggable_type, :scope], length: { name: 100, slug: 20, sluggable_type: 20, scope: 20 }, unique: true
+    add_index :friendly_id_slugs, [:slug, :sluggable_type], length: { slug: 140, sluggable_type: 50 }
+    add_index :friendly_id_slugs, [:slug, :sluggable_type, :scope], length: { slug: 70, sluggable_type: 50, scope: 70 }, unique: true
     add_index :friendly_id_slugs, :sluggable_type
   end
 end
