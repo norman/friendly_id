norman/friendly_id

commit 1f8af74b5f216f9813e5fd844f3a265c48e19d4d

Set slug to previous value on validation failure
On create, set slug to nil if unrelated validations fail.

On save, set slug to previous value if unrelated validations fail.

Without this change, Rails forms will sometimes use a new, unpersisted
slug value as the form action URL, and submitting the form will result
in a 404.

Resolves #642

lib/friendly_id/slugged.rb
@@ -86,7 +86,7 @@ Here's an example of a class that uses a custom method to generate the slug:
     class Person < ActiveRecord::Base
       extend FriendlyId
       friendly_id :name_and_location, use: :slugged
-      
+
       def name_and_location
         "#{name} from #{location}"
       end
@@ -248,6 +248,7 @@ Github issue](https://github.com/norman/friendly_id/issues/185) for discussion.
         defaults[:sequence_separator] ||= '-'
       end
       model_class.before_validation :set_slug
+      model_class.after_validation :unset_slug_if_invalid
     end
 
     # Process the given value to make it suitable for use as a slug.
@@ -323,6 +324,14 @@ Github issue](https://github.com/norman/friendly_id/issues/185) for discussion.
       friendly_id_config.slug_generator_class.new(scope_for_slug_generator)
     end
     private :slug_generator
+
+    def unset_slug_if_invalid
+      if errors.present? && attribute_changed?(friendly_id_config.query_field)
+        diff = changes[friendly_id_config.query_field]
+        self.slug = diff.first
+      end
+    end
+    private :unset_slug_if_invalid
 
     # This module adds the `:slug_column`, and `:sequence_separator`, and
     # `:slug_generator_class` configuration options to

test/slugged_test.rb
@@ -92,6 +92,47 @@ class SluggedTest < TestCaseClass
     end
   end
 
+  test "should not set slug on create if unrelated validations fail" do
+    klass = Class.new model_class do
+      validates_presence_of :active
+      friendly_id :name, :use => :slugged
+
+      def self.name
+        "Journalist"
+      end
+    end
+
+    transaction do
+      instance = klass.new :name => 'foo'
+      refute instance.save
+      refute instance.valid?
+      assert_nil instance.slug
+    end
+  end
+
+  test "should not update slug on save if unrelated validations fail" do
+    klass = Class.new model_class do
+      validates_presence_of :active
+      friendly_id :name, :use => :slugged
+
+      def self.name
+        "Journalist"
+      end
+    end
+
+    transaction do
+      instance = klass.new :name => 'foo', :active => true
+      assert instance.save
+      assert instance.valid?
+      instance.name = 'foobar'
+      instance.slug = nil
+      instance.active = nil
+      refute instance.save
+      refute instance.valid?
+      assert_equal 'foo', instance.slug
+    end
+  end
+
 end
 
 class SlugGeneratorTest < TestCaseClass

commit cb4ddf1fb28bb8199c85a9698df021eb28fa7a1e

Assign to configured slug column, not "slug", when validation fails
1f8af74b mistakenly assigns to the literal `slug` method, rather than
the configured `slug_column`. As a result, models using a custom
`slug_column` get an error raised when validations fail that `slug=` is
an undefined method.

Fixes #765.

lib/friendly_id/slugged.rb
@@ -327,9 +327,9 @@ Github issue](https://github.com/norman/friendly_id/issues/185) for discussion.
     private :slug_generator
 
     def unset_slug_if_invalid
-      if errors.present? && attribute_changed?(friendly_id_config.query_field)
+      if errors.present? && attribute_changed?(friendly_id_config.query_field.to_s)
         diff = changes[friendly_id_config.query_field]
-        self.slug = diff.first
+        send "#{friendly_id_config.slug_column}=", diff.first
       end
     end
     private :unset_slug_if_invalid

test/schema.rb
@@ -67,6 +67,9 @@ module FriendlyId
           # Used to test candidates
           add_column :cities, :code, :string, :limit => 3
 
+          # Used as a non-default slug_column
+          add_column :authors, :subdomain, :string
+
           @done = true
         end
 

test/slugged_test.rb
@@ -110,6 +110,26 @@ class SluggedTest < TestCaseClass
     end
   end
 
+  test "should not set slug on create if unrelated validations fail with custom slug_column" do
+    klass = Class.new(ActiveRecord::Base) do
+      self.table_name = 'authors'
+      extend FriendlyId
+      validates_presence_of :active
+      friendly_id :name, :use => :slugged, :slug_column => :subdomain
+
+      def self.name
+        "Author"
+      end
+    end
+
+    transaction do
+      instance = klass.new :name => 'foo'
+      refute instance.save
+      refute instance.valid?
+      assert_nil instance.subdomain
+    end
+  end
+
   test "should not update slug on save if unrelated validations fail" do
     klass = Class.new model_class do
       validates_presence_of :active