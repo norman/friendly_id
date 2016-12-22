52e9d1c53a834d07982d99583e4413dd7e9c8bb5
Vladimir Kochnev <hashtable@yandex.ru>
2016-12-22 12:14:04 +0300

    Use friendly_id_config.slug_column in unset_slug_if_invalid.

--- lib/friendly_id/slugged.rb
+++ lib/friendly_id/slugged.rb
@@ -329,7 +329,7 @@ Github issue](https://github.com/norman/friendly_id/issues/185) for discussion.
     def unset_slug_if_invalid
       if errors.present? && attribute_changed?(friendly_id_config.query_field)
         diff = changes[friendly_id_config.query_field]
-        self.slug = diff.first
+        send "#{friendly_id_config.slug_column}=", diff.first
       end
     end
     private :unset_slug_if_invalid
