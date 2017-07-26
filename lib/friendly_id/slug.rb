f54ed1434086d456ee9691e183ac9cca5e9a7eeb
Joost van Rijn <joost@joostvanrijn.nl>
2017-07-26 16:03:39 +0200

    Update slug.rb

--- lib/friendly_id/slug.rb
+++ lib/friendly_id/slug.rb
@@ -4,6 +4,10 @@ module FriendlyId
   # @see FriendlyId::History
   class Slug < ActiveRecord::Base
     belongs_to :sluggable, :polymorphic => true
+    
+    def sluggable
+      sluggable_type.constantize.unscoped { super }
+    end
 
     def to_param
       slug
