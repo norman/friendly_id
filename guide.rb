c31997e2a25968f3db00a3506a894c1dfa7b45c9
Amiel Martin <amiel.martin@gmail.com>
2023-04-24 16:45:16 -0700

    Update guide.rb to check for @guide tag

--- guide.rb
+++ guide.rb
@@ -4,8 +4,12 @@
 
 def comments_from path
   path = File.expand_path("../lib/friendly_id/#{path}", __FILE__)
-  match = File.read(path).match(/\n=begin(.*)\n=end/m)[1].to_s
-  match.split("\n").reject { |x| x =~ /^@/ }.join("\n").strip
+  matches = File.read(path).match(/\n# @guide begin(.*)\n# @guide end/m)
+
+  return if matches.nil?
+
+  match = matches[1].to_s
+  match.split("\n").map { |x| x.sub(/^\s*# /, "") }.reject { |x| x =~ /^@/ }.join("\n").strip
 end
 
 File.open(File.expand_path("../Guide.md", __FILE__), "w:utf-8") do |guide|
