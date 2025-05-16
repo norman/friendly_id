c988df5799b75263bbf941ceab93121b24bf8b0f
Shinichi Maeshima <netwillnet@gmail.com>
2025-05-16 12:43:57 +0900

    Update CI matrix to support Rails 8.0 and 7.2, add Ruby 3.3 and 3.4

---
+++ gemfiles/Gemfile.rails-8.0.rb
@@ -0,0 +1,22 @@
+source "https://rubygems.org"
+
+gemspec path: "../"
+
+gem "activerecord", "~> 8.0.0"
+gem "railties", "~> 8.0.0"
+
+# Database Configuration
+group :development, :test do
+  platforms :jruby do
+    gem "activerecord-jdbcmysql-adapter", "~> 61.0"
+    gem "activerecord-jdbcpostgresql-adapter", "~> 61.0"
+    gem "kramdown"
+  end
+
+  platforms :ruby, :rbx do
+    gem "sqlite3"
+    gem "mysql2"
+    gem "pg"
+    gem "redcarpet"
+  end
+end
